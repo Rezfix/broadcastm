3778a63ba03a427063f70217a2bd7d01fb9ec446
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-01-27 15:12:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,7 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
-const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-var prefix = "-";
+var prefix = "!";
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
@@ -1644,33 +1641,5 @@ client.on("guildMemberAdd", (member) => {
             message.channel.send("تم تغيير الاسم الى: " + changenick + "")
         }
     }});
-
-client.on('message', message => {
-var prefix = "-";
- 
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {/Toxic Codes
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {/Toxic Codes
-    if (!args[1]) {
-message.channel.send("**bc <message>**");
-return;
-}
-        message.guild.members.forEach(m => {/Toxic Codes
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .addField('» السيرفر :', `${message.guild.name}`)/Toxic Codes
-            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-            .addField(' » الرسالة : ', args)
-            .setColor('#ff0000')
-            // m.send(`[${m}]`);
-            m.send(`${m}`,{embed: bc});
-        });
-    }
-    } else {/Toxic Codes
-        return;
-    }
-});
 
 client.login(process.env.BOT_TOKEN);
